cb49976bcda9b3873cc4c7c0d73298b70de53e25
booodyb9 <151962053+booodyb9@users.noreply.github.com>
2026-08-07 21:22:26 +0300

    chore: remove remaining obsolete root scripts and restore lockfile

--- patch_seo.py
+++
@@ -1,7 +0,0 @@
-import re
-
-content = open('src/components/SEO.tsx').read()
-content = content.replace("interface SEOProps {", "interface SEOProps {\n  noindex?: boolean;")
-content = content.replace("<link rel=\"alternate\" href={url} hrefLang=\"x-default\" />", "<link rel=\"alternate\" href={url} hrefLang=\"x-default\" />\n      {noindex && <meta name=\"robots\" content=\"noindex, follow\" />}")
-
-open('src/components/SEO.tsx', 'w').write(content)
