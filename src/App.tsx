e98b12f762ef3ac9fb9232751ed228044c0eab89
booodyb9 <151962053+booodyb9@users.noreply.github.com>
2026-08-13 00:36:40 +0300

    fix: disable brand intro while isolating broken homepage rendering

--- src/App.tsx
+++ src/App.tsx
@@ -9,7 +9,6 @@ import FloatingWhatsApp from './components/FloatingWhatsApp';
 import DynamicContactLinks from './components/DynamicContactLinks';
 import HomepageSectionController from './components/HomepageSectionController';
 import DeferredPublicEnhancements from './components/DeferredPublicEnhancements';
-import BrandIntro from './components/BrandIntro';
 
 export default function App() {
   return (
@@ -17,7 +16,6 @@ export default function App() {
       <AuthProvider>
         <ContentProvider>
           <div className="min-h-screen bg-transparent font-sans text-right ltr:text-left relative z-0">
-            <BrandIntro />
             <AmbientBackground />
             <BrowserRouter>
               <ScrollToTop />
